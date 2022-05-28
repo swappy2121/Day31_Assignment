60e581539ce66d329c54c76420c7b15d7681f39c
swapnil <sopiswarrinku77@gmail.com>
2022-05-28 16:11:19 -0700

    Added : MySQL addressBook service user details

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -6,8 +6,8 @@ create table addressBook (firstname varchar(20),lastname varchar(20),address var
 city varchar(20),state varchar(20),zip varchar(6),phone varchar(12),email varchar(20));
 desc addressBook;
 
-insert into addressBook (firstname,lastname,address,city,state,zip,phone,email) values ('Amol','Ghotale','manjari','pune',
-'MH','412307','8657169656','amol9.ag@gmail.com' ),('Priya','Jadhav','ekamba','Bidar','KR','478569','8657169686','priya@gmail.com')
+insert into addressBook (firstname,lastname,address,city,state,zip,phone,email) values ('Swapnil','Pawar','hadapsar','pune',
+'MH','412307','8657169656','amol9.ag@gmail.com' ),('Priya','Jadhav','MJroad','nasik','MH','478569','8657169686','priya@gmail.com')
 ,('Rohit','Ghotale','mudged','Latur','MH','415896','8674521452','rohit@gmail.com');
 select *from addressBook;
 
@@ -23,8 +23,8 @@ select *from addressBook where state = 'MH' order by firstname;
 
 alter table addressBook add column bookname varchar(20), add column booktype varchar(20);
 update addressBook set bookname='Book1' where firstname = 'Swapnil';
-update addressBook set bookname='Book2' where firstname = 'Priya';
-update addressBook set bookname='Book3' where firstname = 'Rohit';
+update addressBook set bookname='Book2' where firstname = 'Rohit';
+update addressBook set bookname='Book3' where firstname = 'Priya';
 update addressBook set booktype='Family' where firstname = 'Swapnil';
 update addressBook set booktype='Friends' where firstname = 'Priya';
 update addressBook set booktype='Profession' where firstname = 'Rohit';
